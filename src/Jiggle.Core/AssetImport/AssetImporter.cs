9809edd1c4b0f24f2b334e5748420c6c5c5fd85e
Marc Dürst <marc@marcduerst.com>
2018-01-06 09:53:36 +0100

    Added basic skeleton for AssetImporter

---
+++ src/Jiggle.Core/AssetImport/AssetImporter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Jiggle.Core.Entities;
+
+namespace Jiggle.Core.AssetImport
+{
+    public class AssetImporter : IAssetImporter
+    {
+        public AssetImporter()
+        {
+        }
+
+        public Task<Asset> ImportAssetAsync(AssetImportOptions options)
+        {
+            if (options == null) throw new ArgumentNullException(nameof(options));
+
+            throw new NotImplementedException();
+        }
+    }
+}
