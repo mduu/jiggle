e481a4b3dfdccf12c9cf7151d60bb3e8bdf5f693
Marc Dürst <marc@marcduerst.com>
2018-01-06 09:55:02 +0100

    Added Jiggle.Core.Tests assembly and a basic test for AssetImporter
    Used this basic test to test running xUnit tests on Visual Studio for Mac

--- src/Jiggle.Core.Tests/AssetImporterTests.cs
+++ src/Jiggle.Core.Tests/AssetImporterTests.cs
@@ -1,14 +1,23 @@
 using System;
 using Xunit;
+using Jiggle.Core.AssetImport;
+using System.Threading.Tasks;
 
 namespace Jiggle.Core.Tests
 {
-    public class UnitTest1
+    public class AssetImporterTests
     {
         [Fact]
-        public void Test1()
+        public async Task ImportWithNullOptions()
         {
+            // Arrage
+            var assetImporter = new AssetImporter();
 
+            // Act
+            var exception = await Assert.ThrowsAsync<ArgumentNullException>(() => assetImporter.ImportAssetAsync(null));
+
+            // Assert
+            Assert.Equal("options", exception.ParamName);
         }
     }
 }
