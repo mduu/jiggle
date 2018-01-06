43df400f1a1e1108e2589b1986f270b5676fa83f
Marc Dürst <marc@marcduerst.com>
2018-01-06 09:45:07 +0100

    Fixed compile error

--- src/Jiggle.Core/AssetImport/IAssetWriter.cs
+++ src/Jiggle.Core/AssetImport/IAssetWriter.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Jiggle.Core.Entities;
+
 namespace Jiggle.Core.AssetImport
 {
     public interface IAssetWriter
     {
-        Task WriteAsset(Asset);
+        Task WriteAsset(Asset asset);
     }
 }
