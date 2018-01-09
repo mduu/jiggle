5979c38865b58aab2afd2ba2e11b0226aaed5356
Marc Dürst <marc@marcduerst.com>
2018-01-09 07:25:21 +0100

    WIP Asset-Importer

--- src/Jiggle.Core/AssetImport/AssetImporter.cs
+++ src/Jiggle.Core/AssetImport/AssetImporter.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Jiggle.Core.Entities;
+using Jiggle.Core.AssetStorage;
 
 namespace Jiggle.Core.AssetImport
 {
     public class AssetImporter : IAssetImporter
     {
-        public AssetImporter()
+        readonly IStoreWriter storeWriter;
+
+        public AssetImporter(IStoreWriter storeWriter)
         {
+            this.storeWriter = storeWriter ?? throw new ArgumentNullException(nameof(storeWriter));
         }
 
         public Task<Asset> ImportAssetAsync(AssetImportOptions options)
