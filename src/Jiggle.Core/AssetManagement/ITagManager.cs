e1610d11cefafd7e8406ef8c66a240ce73d7e1c5
Marc Dürst <marc@marcduerst.com>
2018-01-12 13:20:24 +0100

    Added TagManager and ITagManager

---
+++ src/Jiggle.Core/AssetManagement/ITagManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Jiggle.Core.Entities;
+
+namespace Jiggle.Core.AssetManagement
+{
+    public interface ITagManager
+    {
+        IEnumerable<Tag> GetTagsByName(IEnumerable<string> tagnames);
+    }
+}
---
+++ src/Jiggle.Core/AssetManagement/TagManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Jiggle.Core.Entities;
+using Jiggle.Core.Common;
+using System.Linq;
+
+namespace Jiggle.Core.AssetManagement
+{
+    public class TagManager : ITagManager
+    {
+        readonly DatabaseContext context;
+
+        public TagManager(DatabaseContext context)
+        {
+            this.context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public IEnumerable<Tag> GetTagsByName(IEnumerable<string> tagnames)
+        {
+            if (tagnames == null) throw new ArgumentNullException(nameof(tagnames));
+
+            foreach (var tagname in tagnames)
+            {
+                var tag = context.Tags.FirstOrDefault(t => t.Name.Equals(tagname, StringComparison.CurrentCultureIgnoreCase));
+                if (tag == null)
+                {
+                    tag = new Tag
+                    {
+                        Id = Guid.NewGuid(),
+                        Name = tagname.ToLower(),
+                    };
+
+                    context.Tags.Add(tag);
+                }
+
+                yield return tag;
+            }
+        }
+    }
+}
