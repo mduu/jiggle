2cd453b30b26038f5fa9d9ef6de2dc2810d5c877
Marc Dürst <marc@marcduerst.com>
2018-01-14 07:58:47 +0100

    WIP Thumbnail settings

---
+++ src/Jiggle.Core/Common/IThumbnailSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace Jiggle.Core.Common
+{
+    public interface IThumbnailSettings
+    {
+        /// <summary>
+        /// Gets the width of the thumbnails.
+        /// </summary>
+        /// <value>The width of the thumbnails.</value>
+        int ThumbnailWidth { get; }
+
+        /// <summary>
+        /// Gets the height of the thumbnails.
+        /// </summary>
+        /// <value>The height of the thumbnails.</value>
+        int ThumbnailHeight { get; }
+    }
+}
---
+++ src/Jiggle.Core/Common/JiggleSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Jiggle.Core.Common
+{
+    public class JiggleSettings : IThumbnailSettings
+    {
+        /// <inheritdoc/>
+        public int ThumbnailWidth { get; set; } = 200;
+
+        /// <inheritdoc/>
+        public int ThumbnailHeight { get; set; } = 200;
+    }
+}
