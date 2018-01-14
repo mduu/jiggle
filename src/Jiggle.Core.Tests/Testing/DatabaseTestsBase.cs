00303d31514fd0d3f4ec489c5d7b1df237b777b5
Marc Dürst <marc@marcduerst.com>
2018-01-14 19:33:44 +0100

    Introduced base class for database tests
    It gets the fixture which allows accessing the database
    and begins a new transaction and rollback at after each
    test so the database is fresh and clean.

---
+++ src/Jiggle.Core.Tests/Testing/DatabaseTestsBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace Jiggle.Core.Tests.Testing
+{
+    public abstract class DatabaseTestsBase: IDisposable
+    {
+        protected readonly DatabaseFixture fixture;
+        protected readonly IDbContextTransaction transation;
+
+        public DatabaseTestsBase(DatabaseFixture fixture)
+        {
+            this.fixture = fixture ?? throw new ArgumentNullException(nameof(fixture));
+
+            transation = fixture.DatabaseContext.Database.BeginTransaction();
+        }
+
+        public void Dispose()
+        {
+            transation?.Rollback();
+        }
+
+    }
+}
