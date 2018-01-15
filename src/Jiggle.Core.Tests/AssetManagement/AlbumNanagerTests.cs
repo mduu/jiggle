142f61e13cbbbefd092c88cd785b4dc2e10dfe8b
Marc Dürst <marc@marcduerst.com>
2018-01-15 13:14:14 +0100

    Added more unit-tests for AlbumManager

--- src/Jiggle.Core.Tests/AssetManagement/AlbumNanagerTests.cs
+++ src/Jiggle.Core.Tests/AssetManagement/AlbumNanagerTests.cs
@@ -6,6 +6,7 @@ using Jiggle.Core.Entities;
 using Jiggle.Core.Tests.Testing;
 using Xunit;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace Jiggle.Core.Tests.AssetManagement
 {
@@ -13,20 +14,22 @@ namespace Jiggle.Core.Tests.AssetManagement
     public class AlbumNanagerTests : DatabaseTestsBase
     {
         private readonly AlbumManager albumManager;
+        private readonly Album album1;
+        private readonly Album album2;
 
         public AlbumNanagerTests(DatabaseFixture fixture)
             : base(fixture)
         {
             albumManager = new AlbumManager(fixture.DatabaseContext);
+            album1 = CreateAlbum("My Album 1", new[] { "My Childalbum 1", "My Childalbum 2" });
+            album2 = CreateAlbum("My Album 2");
+            fixture.DatabaseContext.SaveChanges();
         }
 
         [Fact]
         public async Task Test_GetAlbumByIdAsync()
         {
             // Arrange
-            var album1 = CreateAlbum("My Album 1", new[] { "My Childalbum 1", "My Childalbum 2" });
-            var album2 = CreateAlbum("My Album 2");
-            fixture.DatabaseContext.SaveChanges();
 
             // Act
             var album = await albumManager.GetAlbumByIdAsync(album1.Id);
@@ -37,6 +40,112 @@ namespace Jiggle.Core.Tests.AssetManagement
             Assert.NotNull(album.CreatedBy);
             Assert.NotNull(album.ChildAlbums);
             Assert.Equal(2, album.ChildAlbums.Count);
+        }
+
+        [Fact]
+        public async Task Test_GetAlbumByIdAsync_InvalidId()
+        {
+            // Arrange
+
+            // Act
+            await Assert.ThrowsAsync<InvalidOperationException>(() => albumManager.GetAlbumByIdAsync(Guid.NewGuid()));
+
+            // Assert
+        }
+
+        [Fact]
+        public async Task Test_GetAlbumsByParentAlbumIdAsync()
+        {
+            // Arrange
+
+            // Act
+            var childAlbums = (await albumManager.GetAlbumsByParentAlbumIdAsync(album1.Id))
+                .ToList();
+
+            // Assert
+            Assert.NotEmpty(childAlbums);
+            Assert.Equal(2, childAlbums.Count());
+            Assert.Equal(album1.Id, childAlbums[0].ParentAlbumId);
+            Assert.Equal(album1.Id, childAlbums[1].ParentAlbumId);
+        }
+
+        [Fact]
+        public async Task Test_GetAlbumsByParentAlbumIdAsync_InvalidParent()
+        {
+            // Arrange
+
+            // Act
+            var childAlbums = (await albumManager.GetAlbumsByParentAlbumIdAsync(Guid.NewGuid()))
+                .ToList();
+
+            // Assert
+            Assert.Empty(childAlbums);
+        }
+
+        [Fact]
+        public void Test_CreateNewAlbum_WithoutParent()
+        {
+            // Arrange
+            const string albumname = "New Album";
+            const string albumdescription = "Albumdescription";
+            var user = fixture.DatabaseContext.Users.First();
+
+            // Act
+            var album = albumManager.CreateNewAlbum(
+                albumname,
+                albumdescription,
+                user.Id);
+            fixture.DatabaseContext.SaveChanges();
+
+            // Assert
+            Assert.NotNull(album);
+            Assert.Equal(albumname, album.Name);
+            Assert.Equal(albumdescription, album.Description);
+            Assert.Equal(user.Id, album.CreatedById);
+        }
+
+        [Fact]
+        public void Test_CreateNewAlbum_WithParent()
+        {
+            // Arrange
+            const string albumname = "New Album";
+            const string albumdescription = "Albumdescription";
+            var user = fixture.DatabaseContext.Users.First();
+
+            // Act
+            var album = albumManager.CreateNewAlbum(
+                albumname,
+                albumdescription,
+                user.Id,
+                album1.Id);
+            fixture.DatabaseContext.SaveChanges();
+
+            // Assert
+            Assert.NotNull(album);
+            Assert.Equal(albumname, album.Name);
+            Assert.Equal(albumdescription, album.Description);
+            Assert.Equal(user.Id, album.CreatedById);
+            Assert.Equal(album1.Id, album.ParentAlbumId);
+        }
+
+        [Fact]
+        public void Test_CreateNewAlbum_With_Invalid_Parent()
+        {
+            // Arrange
+            const string albumname = "New Album";
+            const string albumdescription = "Albumdescription";
+            var user = fixture.DatabaseContext.Users.First();
+
+            // Act
+            var album = albumManager.CreateNewAlbum(
+                albumname,
+                albumdescription,
+                user.Id,
+                Guid.NewGuid());
+            
+            Assert.Throws<DbUpdateException>(() => fixture.DatabaseContext.SaveChanges());
+
+            // Assert
         }
 
         private Album CreateAlbum(string albumName, IEnumerable<string> childAlbumNames = null)
