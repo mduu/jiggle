9820cab702e9d41ba0b152d864dae27ed84da90f
Marc Dürst <marc@marcduerst.com>
2018-01-18 14:47:12 +0100

    Reverted WeatherForecasts.ts

--- src/Jiggle.Server/ClientApp/store/WeatherForecasts.ts
+++ src/Jiggle.Server/ClientApp/store/WeatherForecasts.ts
@@ -5,50 +5,54 @@ import { AppThunkAction } from './';
 // -----------------
 // STATE - This defines the type of data maintained in the Redux store.
 
-export interface UploadState {
+export interface WeatherForecastsState {
     isLoading: boolean;
-    existingAlbums: Album[];
+    startDateIndex?: number;
+    forecasts: WeatherForecast[];
 }
 
-export interface Album {
-    id: string;
-    level: number;
-    name: number;
+export interface WeatherForecast {
+    dateFormatted: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
 }
 
 // -----------------
 // ACTIONS - These are serializable (hence replayable) descriptions of state transitions.
 // They do not themselves have any side-effects; they just describe something that is going to happen.
 
-interface RequestUploadAction {
-    type: 'REQUEST_UPLOAD_INFO';
+interface RequestWeatherForecastsAction {
+    type: 'REQUEST_WEATHER_FORECASTS';
+    startDateIndex: number;
 }
 
-interface ReceiveUploadAction {
-    type: 'RECEIVE_UPLOAD_INFO';
-    existingAlbums: Album[];
+interface ReceiveWeatherForecastsAction {
+    type: 'RECEIVE_WEATHER_FORECASTS';
+    startDateIndex: number;
+    forecasts: WeatherForecast[];
 }
 
 // Declare a 'discriminated union' type. This guarantees that all references to 'type' properties contain one of the
 // declared type strings (and not any other arbitrary string).
-type KnownAction = RequestUploadAction | ReceiveUploadAction;
+type KnownAction = RequestWeatherForecastsAction | ReceiveWeatherForecastsAction;
 
 // ----------------
 // ACTION CREATORS - These are functions exposed to UI components that will trigger a state transition.
 // They don't directly mutate state, but they can have external side-effects (such as loading data).
 
 export const actionCreators = {
-    requestUpload: (startDateIndex: number): AppThunkAction<KnownAction> => (dispatch, getState) => {
+    requestWeatherForecasts: (startDateIndex: number): AppThunkAction<KnownAction> => (dispatch, getState) => {
         // Only load data if it's something we don't already have (and are not already loading)
-        if (!existingAlbums) {
-            let fetchTask = fetch(`api/Asset/Upload`)
-                .then(response => response.json() as Promise<Album[]>)
+        if (startDateIndex !== getState().weatherForecasts.startDateIndex) {
+            let fetchTask = fetch(`api/SampleData/WeatherForecasts?startDateIndex=${ startDateIndex }`)
+                .then(response => response.json() as Promise<WeatherForecast[]>)
                 .then(data => {
-                    dispatch({ type: 'RECEIVE_UPLOAD_INFO', existingAlbums: data });
+                    dispatch({ type: 'RECEIVE_WEATHER_FORECASTS', startDateIndex: startDateIndex, forecasts: data });
                 });
 
             addTask(fetchTask); // Ensure server-side prerendering waits for this to complete
-            dispatch({ type: 'REQUEST_UPLOAD_INFO' });
+            dispatch({ type: 'REQUEST_WEATHER_FORECASTS', startDateIndex: startDateIndex });
         }
     }
 };
@@ -56,23 +60,27 @@ export const actionCreators = {
 // ----------------
 // REDUCER - For a given state and action, returns the new state. To support time travel, this must not mutate the old state.
 
-const unloadedState: UploadState = { existingAlbums: [], isLoading: false };
+const unloadedState: WeatherForecastsState = { forecasts: [], isLoading: false };
 
-export const reducer: Reducer<UploadState> = (state: UploadState, incomingAction: Action) => {
+export const reducer: Reducer<WeatherForecastsState> = (state: WeatherForecastsState, incomingAction: Action) => {
     const action = incomingAction as KnownAction;
     switch (action.type) {
-        case 'REQUEST_UPLOAD_INFO':
+        case 'REQUEST_WEATHER_FORECASTS':
             return {
-                existingAlbums: state.existingAlbums,
+                startDateIndex: action.startDateIndex,
+                forecasts: state.forecasts,
                 isLoading: true
             };
-        case 'RECEIVE_UPLOAD_INFO':
+        case 'RECEIVE_WEATHER_FORECASTS':
             // Only accept the incoming data if it matches the most recent request. This ensures we correctly
             // handle out-of-order responses.
-            return {
-                existingAlbums: action.existingAlbums,
-                isLoading: false
-            };
+            if (action.startDateIndex === state.startDateIndex) {
+                return {
+                    startDateIndex: action.startDateIndex,
+                    forecasts: action.forecasts,
+                    isLoading: false
+                };
+            }
             break;
         default:
             // The following line guarantees that every action in the KnownAction union has been covered by a case above
