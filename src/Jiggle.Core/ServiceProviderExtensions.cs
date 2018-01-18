8710f278ed3c9f370b096b15b59e648a141119c1
Marc Dürst <marc@marcduerst.com>
2018-01-18 19:33:54 +0100

    WIP: Dependency Injection Registration for the Jiggle Core

---
+++ src/Jiggle.Core/ServiceProviderExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Jiggle.Core
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void AddJiggleCore(this IServiceProvider serviceProvider)
+        {
+            if (serviceProvider == null) throw new ArgumentNullException(nameof(serviceProvider));
+
+        }
+    }
+}
