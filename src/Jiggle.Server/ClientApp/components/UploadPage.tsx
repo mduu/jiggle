9f08cea36a43d22c7a94842b3a64618933945877
Marc Dürst <marc@marcduerst.com>
2018-01-21 12:52:10 +0100

    Removed Redux Store from Upload page

---
+++ src/Jiggle.Server/ClientApp/components/UploadPage.tsx
@@ -0,0 +1,40 @@
+﻿import * as React from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+
+// At runtime, Redux will merge together...
+type UploadProps =
+    RouteComponentProps<{}>; // ... plus incoming routing parameters
+
+class UploadPage extends React.Component<UploadProps, {}> {
+    componentWillMount() {
+        // This method runs when the component is first added to the page
+
+        //let fetchTask = fetch(`api/Upload/InitialData`)
+        //    .then(response => response.json() as Promise<Album[]>)
+        //    .then(data => {
+        //        dispatch({ type: 'RECEIVE_UPLOAD_INITIAL_DATA', existingAlbums: data });
+        //    });
+    }
+
+    componentWillReceiveProps(nextProps: UploadProps) {
+        // This method runs when incoming props (e.g., route params) change
+    }
+
+    public render() {
+        return <div>
+            <h1>Upload assets</h1>
+            <p>In this screen one can upload new assets like images and videos.</p>
+            { this.renderUploadForm() }
+        </div>;
+    }
+
+    private renderUploadForm() {
+        return <form>
+            <label>Existing albums:</label>
+            <select>
+            </select>
+        </form>
+    }
+}
+
+export default UploadPage;
--- src/Jiggle.Server/ClientApp/routes.tsx
+++ src/Jiggle.Server/ClientApp/routes.tsx
@@ -4,11 +4,11 @@ import { Layout } from './components/Layout';
 import Home from './components/Home';
 import FetchData from './components/FetchData';
 import Counter from './components/Counter';
-import Upload from './components/Upload';
+import UploadPage from './components/UploadPage';
 
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
-    <Route path='/upload' component={ Upload } />
+    <Route path='/upload' component={ UploadPage } />
     <Route path='/counter' component={ Counter } />
     <Route path='/fetchdata/:startDateIndex?' component={ FetchData } />
 </Layout>;
