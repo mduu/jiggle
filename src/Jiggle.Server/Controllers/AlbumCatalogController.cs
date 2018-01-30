3f2dbde92217a9ac782a66f98714d3b48c3f5ac8
Marc Dürst <marc@marcduerst.com>
2018-01-30 12:06:59 +0100

    Added basic WebAPI Core app

---
+++ src/Jiggle.Server/Controllers/AlbumCatalogController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Jiggle.Core.AssetManagement;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jiggle.Server.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/AlbumCatalog")]
+    public class AlbumCatalogController : Controller
+    {
+        private readonly IAlbumManager _albumManager;
+
+        public AlbumCatalogController(
+            IAlbumManager albumManager)
+        {
+            _albumManager = albumManager ?? throw new ArgumentNullException(nameof(albumManager));
+        }
+
+        // GET api/albumcatalog
+        [HttpGet]
+        public async Task<IEnumerable<AlbumCatalogController.Album>> Get()
+        {
+            var existingAlbums = await _albumManager.GetAllNewestAlbumsAsync();
+
+            return existingAlbums.Select(a => new Album
+            {
+                AlbumId = a.Id,
+                AlnumName = a.Name,
+            });
+        }
+
+        public class Album
+        {
+            public Guid AlbumId { get; set; }
+            public string AlnumName { get; set; }
+        }
+    }
+}
---
+++ src/Jiggle.Server/Startup.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Jiggle.Core;
+using Jiggle.Core.AssetManagement.FileStore;
+using Jiggle.Core.Common;
+using Jiggle.Server.Core;
+
+namespace Jiggle.Server
+{
+    public class Startup
+    {
+        public Startup(
+            IHostingEnvironment env,
+            IConfiguration configuration)
+        {
+            CurrentEnvironment = env;
+            Configuration = configuration;
+        }
+
+        public IHostingEnvironment CurrentEnvironment { get; }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlite("Data Source=Jiggle.db"));
+
+            services.AddJiggleCore(
+                new ThumbnailSettings(200, 200), // TODO
+                new FileSystemConfiguration(
+                    CurrentEnvironment.MapPath(Configuration["AssetManagement:OriginalsRootPath"]),
+                    CurrentEnvironment.MapPath(Configuration["AssetManagement:ThumbnailsRootPath"])
+                )
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
