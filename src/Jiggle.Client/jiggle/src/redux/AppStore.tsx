814dd6772689c0654c7cce266cd0c894de0748f7
Marc Dürst <marc@marcduerst.com>
2018-02-03 20:46:02 +0100

    Added Redux store for main app

---
+++ src/Jiggle.Client/jiggle/src/redux/AppStore.tsx
@@ -0,0 +1,35 @@
+// --- State ---
+export interface AppStoreState {
+    selectedMainMenuItem: string;
+}
+
+// --- Actions ----
+export const MAINMENU_SELECTITEM = 'MAINMENU_SELECTITEM';
+export type MAINMENU_SELECTITEM = typeof MAINMENU_SELECTITEM;
+
+// tslint:disable-next-line:class-name
+export interface MainMenu_SelectItem {
+    type: MAINMENU_SELECTITEM;
+    selectedMainMenuItem: string;
+}
+
+// MainMenu Actions Type
+export type MainMenuAction = MainMenu_SelectItem /*| DecrementEnthusiasm */;
+
+// --- Action Factories ---
+export function mainMenuSelectItem(selectedMainMenuItem: string): MainMenu_SelectItem {
+    return {
+        type: MAINMENU_SELECTITEM,
+        selectedMainMenuItem: selectedMainMenuItem
+    };
+}
+
+// --- Reducers ---
+export function enthusiasm(state: AppStoreState, action: MainMenuAction): AppStoreState {
+    switch (action.type) {
+        case MAINMENU_SELECTITEM:
+            return { ...state, selectedMainMenuItem: action.selectedMainMenuItem };
+        default:
+            return state;
+    }
+}
