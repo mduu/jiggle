8e9ac8ee2c569ca98a2ea943e35d51ae80b94cbf
Marc Dürst <marc@marcduerst.com>
2018-02-03 07:54:28 +0100

    Implemented basic page layout
    Layout with sidebar for navigation and content area.
    Both using the maxiumum height.

--- src/Jiggle.Client/jiggle/src/components/App/App.tsx
+++ src/Jiggle.Client/jiggle/src/components/App/App.tsx
@@ -6,13 +6,19 @@ const logo = require('./logo.svg');
 const App = ({}) => (
 
   <div className="App">
-    <header className="App-header">
+    <aside className="sidebar-menu">
       <img src={logo} className="App-logo" alt="logo" />
-      <h1 className="App-title">Welcome to React</h1>
-    </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.tsx</code> and save to reload.
-    </p>
+      <h2>Menu</h2>
+    </aside>
+
+    <section className="main-content">
+
+      <h1 className="App-title">Welcome to Jiggle</h1>
+      <p>
+        To get started, edit <code>src/App.tsx</code> and save to reload.
+      </p>
+
+    </section>
   </div>
 );
 
