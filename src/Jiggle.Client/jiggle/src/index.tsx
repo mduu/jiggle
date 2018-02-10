463dde8185e7a5c722f1c66fb2ecdcbb369e8cf4
Marc Dürst <marc@marcduerst.com>
2018-02-10 07:39:19 +0100

    WIP: Adding react-router

--- src/Jiggle.Client/jiggle/src/index.tsx
+++ src/Jiggle.Client/jiggle/src/index.tsx
@@ -4,11 +4,34 @@ import { Provider } from 'react-redux';
 import App from './components/App/App';
 import { appStore } from './redux/AppStore';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
+import { mainMenu } from './redux/AppStore';
 import './index.css';
+
+// Create a history of your choosing (we're using a browser history in this case)
+const history = createHistory();
+
+// Build the middleware for intercepting and dispatching navigation actions
+const middleware = routerMiddleware(history);
+
+// Add the reducer to your store on the `router` key
+// Also apply our middleware for navigating
+const store = createStore(
+  combineReducers({
+    ...mainMenu,
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
+);
 
 ReactDOM.render(
   <Provider store={appStore}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('root') as HTMLElement
 );
