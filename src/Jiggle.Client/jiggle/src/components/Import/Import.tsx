908510c7180753671adbbd4a2069dda40070116c
Marc Dürst <marc@marcduerst.com>
2018-02-26 13:29:42 +0100

    Import: WIP Basic form

--- src/Jiggle.Client/jiggle/src/components/Import/Import.tsx
+++ src/Jiggle.Client/jiggle/src/components/Import/Import.tsx
@@ -5,7 +5,14 @@ const Import = ({}) => (
 
   <section className="Import">
     <h1>Import Assets</h1>
-    <p>Under constuction...</p>
+    <p>
+      Upload / Import your asset like images into a new or existing album. You can also add tags (keywords) so you
+      easiliy find it later on.
+    </p>
+    <form>
+      <label htmlFor="newalbumname">New album name:</label>
+      <input id="newalbumname" type="text" placeholder="Name of the new album" />
+    </form>
   </section>
 );
 
