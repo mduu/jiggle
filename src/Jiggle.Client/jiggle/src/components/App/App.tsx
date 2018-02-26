452e92bf941ccd39db2d081b6067df37a7cf2e55
Marc Dürst <marc@marcduerst.com>
2018-02-26 10:19:25 +0100

    Added page-switcher and import sub page

--- src/Jiggle.Client/jiggle/src/components/App/App.tsx
+++ src/Jiggle.Client/jiggle/src/components/App/App.tsx
@@ -1,21 +1,49 @@
 import * as React from 'react';
+import { connect, Dispatch } from 'react-redux';
+import * as AppStore from '../../redux/AppStore';
 import Menu from './Menu';
+import Import from '../Import/Import';
 import './App.css';
 
-const App = ({}) => (
+const mainContant = (selectedMainMenuItem: string) => {
+  switch (selectedMainMenuItem) {
+    case 'import':
+        return <Import />;
+    default:
+        return (
+          <section>
+            <h1 className="App-title">Welcome to Jiggle</h1>
+            <p>
+              Later on here will be an activtiy stream of Jiggle.
+            </p>
+          </section>
+        );
+  }
+};
+
+export interface AppProps {
+  selectedMainMenuItem: string;
+}
+
+const App = (props: AppProps) => (
 
   <div className="App">
     <Menu />
 
     <section className="main-content">
-
-      <h1 className="App-title">Welcome to Jiggle</h1>
-      <p>
-        To get started, edit <code>src/App.tsx</code> and save to reload.
-      </p>
-
+      {mainContant(props.selectedMainMenuItem)}
     </section>
   </div>
 );
 
-export default App;
+export function mapStateToProps({ selectedMainMenuItem }: AppStore.AppStoreState) {
+  return {
+      selectedMainMenuItem
+  };
+}
+
+export function mapDispatchToProps(dispatch: Dispatch<AppStore.MainMenuAction>) {
+  return {  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/Jiggle.Client/jiggle/src/components/Import/Import.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './Import.css';
+
+const Import = ({}) => (
+
+  <section className="Import">
+    <h1>Import Assets</h1>
+
+  </section>
+);
+
+export default Import;
