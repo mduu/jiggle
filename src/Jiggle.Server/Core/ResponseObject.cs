a62567e095dc99b0dda92bdd93e8f06f735923f8
Marc Dürst <marc@marcduerst.com>
2018-05-22 07:15:47 +0200

    WIP: Load initial masterdata

---
+++ src/Jiggle.Server/Core/ResponseObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Jiggle.Server.Communication
+{
+    public class ResponseObject<TPayload>
+    {
+        public ResponseObject()
+        {
+        }
+
+        public ResponseObject(TPayload payload) => Payload = payload;
+
+        public ResponseObject(IEnumerable<ServerError> errors) => Errors = errors;
+
+        [JsonProperty("payload")]
+        public TPayload Payload { get; set; }
+
+        [JsonProperty("errors")]
+        public IEnumerable<ServerError> Errors { get; set; }
+    }
+}
---
+++ src/Jiggle.Server/Core/ServerError.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Jiggle.Server.Communication
+{
+    public class ServerError
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("errorCode")]
+        public string ErrorCode { get; set; }
+
+        [JsonProperty("fieldName")]
+        public string FIeldName { get; set; }
+    }
+}
