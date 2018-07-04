909c8d9ca8c54a17bede59569b6447120889f0c1
Marc Dürst <marc@marcduerst.com>
2018-07-04 06:53:39 +0200

    Fixwd compiler warnings

--- src/Jiggle.Client/jiggle/src/elements/ErrorMessage/ErrorMessage.tsx
+++ src/Jiggle.Client/jiggle/src/elements/ErrorMessage/ErrorMessage.tsx
@@ -12,7 +12,7 @@ export interface IProps {
     error: IError;
 }
 
-const formatErrorMessage = (error: IError): string {
+const formatErrorMessage = (error: IError): string => {
     let result = error.message;
 
     let msgFragments = [];
@@ -20,10 +20,10 @@ const formatErrorMessage = (error: IError): string {
         msgFragments.push('Code: ' + error.code);
     }
     if (error.serverCode) {
-        msgFragments.push('Server-Code: ' + error.serverCode)
+        msgFragments.push('Server-Code: ' + error.serverCode);
     }
     if (error.fieldName) {
-        msgFragments.push('Fieldname: ' + error.fieldName)
+        msgFragments.push('Fieldname: ' + error.fieldName);
     }
 
     if (msgFragments.length > 0) {
@@ -31,23 +31,22 @@ const formatErrorMessage = (error: IError): string {
     }
 
     return result;
-}
+};
 
-export const ErrorMessage = ({error}: IProps) =>
-    (
-        <Snackbar
-            className={styles.errorMessageRoot}
-            open={true}
-            anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'center',
-            }}
-            ContentProps={{
-                'aria-describedby': 'message-id',
-            }}
-            message={
-                <span id="message-id">
+export const ErrorMessage = ({error}: IProps) => (
+    <Snackbar
+        className={styles.errorMessageRoot}
+        open={true}
+        anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'center',
+        }}
+        ContentProps={{
+            'aria-describedby': 'message-id',
+        }}
+        message={
+            <span id="message-id">
                     {formatErrorMessage(error)}
                 </span>}
-        />
-    );
+    />
+);
